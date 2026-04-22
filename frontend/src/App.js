0342078dda7225397dd671fd084b88c6dc3344a4
Nozs002 <phamson26082005@gmail.com>
2026-04-22 23:44:51 +0700

    init base with nodejsnodejs

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>Chào mừng đến với hệ thống SmartVibe!</h1>
+      <p>Tôi đã cài đặt thành công React.</p>
+    </div>
+  );
+}
+
+export default App;
